d232c297cb598c4d11942cfdcc79c0d89da26a47
Neda <nedaojaghlou@gmail.com>
2015-06-23 09:44:59 -0400

    add bad temp

---
+++ src/temperatu.py
@@ -0,0 +1,9 @@
+#! /user/bin/env python
+"""this is python teprurature
+"""
+def f_to_k(temp):
+    converted = ((temp - 32) * (5/9)) + 273.15
+    return converted
+
+print(f_to_k(32))
+print(f_to_k(212))
